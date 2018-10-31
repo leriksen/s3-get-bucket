38822c135300a8b5557c15dde915c95db8fed2ba
Leif Eriksen <leif.eriksen.au@gmail.com>
2018-10-31 21:29:01 +1100

    get bucket loc

---
+++ s3loc.js
@@ -0,0 +1,13 @@
+const AWS = require('aws-sdk');
+
+const S3 = new AWS.S3({apiVersion: '2006-03-01'});
+
+S3.getBucketLocation(
+    {Bucket: 'kms-secrets-example-bucket'},
+    (err, data) => {
+        if (err)
+            console.log(err, err.stack);
+        else
+            console.log(data);
+    }
+);
